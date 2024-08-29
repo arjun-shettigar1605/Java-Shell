d55d7e8151d7698e4df9caa42c6ca449d75ea9b5
arjun-shettigar1605 <arjun.shettigar16@gmail.com>
2024-08-30 00:52:32 +0530

    pass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,14 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if(input.equals("exit 0"))
-                break;
-            if(input.startsWith("echo"))
-                System.out.println(input.substring(5));
+           
             if(input.startsWith("type"))
             {
-                if(input.substring(5)=="echo" || input.substring(5)=="type" || input.substring(5)=="exit")
+                if(input.substring(5).equals("echo") || input.substring(5).equals("type") || input.substring(5).equals("exit"))
                 {
                     System.out.println(input.substring(5)+" is a shell builtin");
                 }
-                else if(input.substring(5)=="cat")
+                else if(input.substring(5).equals("cat"))
                 {
                     System.out.println(input.substring(5)+" is /bin/cat");
                 }
