6566f89ed1be600f3dcb231abfbac94c7592c953
arjun-shettigar1605 <arjun.shettigar16@gmail.com>
2024-08-30 00:47:52 +0530

    pass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,21 @@ public class Main {
                 break;
             if(input.startsWith("echo"))
                 System.out.println(input.substring(5));
-            else
-                System.out.println(input+": command not found");
+            if(input.startsWith("type"))
+            {
+                if(input.substring(5)=="echo" || input.substring(5)=="type" || input.substring(5)=="exit")
+                {
+                    System.out.println(input.substring(5)+" is a shell builtin");
+                }
+                else if(input.substring(5)=="cat")
+                {
+                    System.out.println(input.substring(5)+" is /bin/cat");
+                }
+                else
+                {
+                    System.out.println(input.substring(5)+" not found");
+                }
+            }
         }
         
     }
