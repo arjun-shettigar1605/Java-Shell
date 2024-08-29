9d8d01567c75278850f2e1f735490071a3fe03c2
arjun-shettigar1605 <arjun.shettigar16@gmail.com>
2024-08-30 00:54:03 +0530

    pass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,10 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-           
+            if(input.equals("exit 0"))
+                break;
+            if(input.startsWith("echo"))
+                System.out.println(input.substring(5));
             if(input.startsWith("type"))
             {
                 if(input.substring(5).equals("echo") || input.substring(5).equals("type") || input.substring(5).equals("exit"))
@@ -24,6 +27,5 @@ public class Main {
                 }
             }
         }
-        
     }
 }
