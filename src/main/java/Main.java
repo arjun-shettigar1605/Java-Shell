61466140869ddefa83320cec28334235b2096bc9
arjun-shettigar1605 <arjun.shettigar16@gmail.com>
2024-08-30 00:36:01 +0530

    pass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
         while(true)
         {
             System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if(input.equals("exit 0"))
-                break;
-            System.out.println(input+": command not found");
+            if(input.startWith("echo"))
+                System.out.println(input.substring(5));
+            else
+                System.out.println(input+": command not found");
         }
         
     }
