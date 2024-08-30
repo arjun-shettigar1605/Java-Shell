c13b965eb1f95595b7be2dc2c46e3f9f97de4e16
arjun-shettigar1605 <arjun.shettigar16@gmail.com>
2024-08-30 19:33:38 +0530

    pass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        while(true)
+        boolean flag=true;
+        while(flag==true)
         {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if(input.equals("exit 0") || input.matches(""))
+            if(input.equals("exit 0"))
                 break;
+            if(input.matches(""))
+                flag=false;
             if(input.startsWith("echo"))
                 System.out.println(input.substring(5));
             if(input.startsWith("type"))
