f57d4b073f42abc012b668f520a0e62fd40974e6
arjun-shettigar1605 <arjun.shettigar16@gmail.com>
2024-08-30 22:24:21 +0530

    pass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        boolean flag=true;
+        // boolean flag=true;
         while(flag==true)
         {
             System.out.print("$ ");
